8a3f7b58a5c64819df02499b063b2ab6be3a8558
Anwesh43 <anweshthecool0@gmail.com>
2022-01-10 01:04:07 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.01
+const sizeFactor : number = 11.2 
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
@@ -35,7 +36,7 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
@@ -43,4 +44,37 @@ export const useDimension = () => {
         w, 
         h, 
     }
+}
+
+const maxScale = (scale : number, i : number, n : number) => Math.max(0, scale - i / n)
+const divideScale = (scale : number, i : number, n : number) => Math.min(1 / n, maxScale(scale, i, n)) * n 
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / sizeFactor 
+    const sf : number = sinify(scale)
+    const sc1 : number = divideScale(sf, 0, 2)
+    const sc2 : number = divideScale(sf, 1, 2)
+    const position = 'absolute'
+    const background = 'indigo'
+    const width = `${size}px`
+    const height = `${size}px`
+    return {
+        blockStyle(i : number) : CSSProperties {
+            const left : string = `${w / 2 - (1 - 2 * i) * (w / 2 - size / 2) * sc2}px`
+            const top : string = `${h - size - (h / 2 - size / 2) * sc1}px`
+            const borderRadius : string = `50%`
+            return {
+                background, 
+                position, 
+                width, 
+                height,
+                left,
+                top,
+                borderRadius
+            }
+
+        }
+    }
+    
 }
